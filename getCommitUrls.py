054ffcaf5a5a1b378385be1337c448d7cfd22dcf
Yug <03yugagarwal@gmail.com>
2023-09-13 22:23:29 +0530

    fixed bug

--- getCommitUrls.py
+++ getCommitUrls.py
@@ -4,11 +4,11 @@ try:
     from bs4 import BeautifulSoup
 except Exception as e:
     print('Modules Missing {}'.format(e))
-file1=open('./next_link.txt','r')
-file2=open('./href.txt','w')
-while(True):
-    url=file1.readline()
-    if(url==""):
+file1 = open('./next_link.txt', 'r')
+file2 = open('./href.txt', 'w')
+while (True):
+    url = file1.readline()
+    if (url == ""):
         break
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -21,7 +21,6 @@ while(True):
 
     for i in href:
         print(i)
-        file2.write(i+"\n")
+        file2.write(i + "\n")
 file1.close()
 file2.close()
-    
--- getData.py
+++ getData.py
@@ -7,22 +7,23 @@ file1=open('./href.txt','r')
 file2=open('./commit_metrics.txt','w')
 #url = "https://github.com/apache/dubbo/commit/8a509e9601fab4af278859c9d947fca8f5f0fa27"
 while(True):
-    url=file1.readline()
+    url=file1.readline().strip()
     if(url==""):
         break
     url="https://github.com"+url
     print(url)
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
-
     a = soup.find('div', class_='commit-title markdown-title')
     print(a)
     b = soup.find('div', class_='commit-desc')
-    b = b.find()
+    b = b.find('pre')
     print(b)
     c = soup.find('div', class_='d-flex flex-items-center js-details-container Details flex-1')
     c = c.findAll('strong')
     print(c)
+    d = soup.find('relative-item')
+    print(d)
     file2.write(a+"\n"+b+"\n"+c+"\n")
 file1.close()
 file2.close()
