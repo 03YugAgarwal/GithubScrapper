a605219696ea3e50e13c8467675de95fe5c6f813
Aviral Singh <aviral.singh2021@vitstudent.ac.in>
2023-09-13 23:18:05 +0530

    Fixed write issue in getData.py

--- getData.py
+++ getData.py
@@ -3,27 +3,36 @@ try:
     from bs4 import BeautifulSoup
 except Exception as e:
     print('Modules Missing {}'.format(e))
-file1=open('./href.txt','r')
-file2=open('./commit_metrics.txt','w')
-#url = "https://github.com/apache/dubbo/commit/8a509e9601fab4af278859c9d947fca8f5f0fa27"
-while(True):
-    url=file1.readline().strip()
-    if(url==""):
+
+file1 = open('./href.txt', 'r')
+file2 = open('./commit_metrics.txt', 'w')
+
+while True:
+    url = file1.readline().strip()
+    if url == "":
         break
-    url="https://github.com"+url
+    url = "https://github.com" + url
     print(url)
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    a = soup.find('div', class_='commit-title markdown-title')
-    print(a)
-    b = soup.find('div', class_='commit-desc')
-    b = b.find('pre')
-    print(b)
-    c = soup.find('div', class_='d-flex flex-items-center js-details-container Details flex-1')
-    c = c.findAll('strong')
-    print(c)
-    d = soup.find('relative-item')
-    print(d)
-    file2.write(a+"\n"+b+"\n"+c+"\n")
+    
+    try:
+        r = requests.get(url)
+        r.raise_for_status()  # Check if the request was successful
+        soup = BeautifulSoup(r.text, 'html.parser')
+        
+        a = soup.find('div', class_='commit-title markdown-title').get_text()
+        b = soup.find('div', class_='commit-desc').find('pre').get_text()
+        
+        c_container = soup.find('div', class_='d-flex flex-items-center js-details-container Details flex-1')
+        c_elements = c_container.findAll('strong')
+        c_texts = [element.get_text() for element in c_elements]
+        c = "\n".join(c_texts)
+        
+        file2.write(a + "\n" + b + "\n" + c + "\n")
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Request error for {url}: {e}")
+    except Exception as e:
+        print(f"Error processing {url}: {e}")
+
 file1.close()
 file2.close()
